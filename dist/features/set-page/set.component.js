9a2510a2dc7246a3ceb59395d29687c665b84018
K2x141b <xaviencedric@gmail.com>
2025-10-11 15:09:49 +0200

    configuring Vercel

--- dist/features/set-page/set.component.js
+++
@@ -1,8 +0,0 @@
-import { toggleReverseBtn } from "./set.logic.js";
-import { displayReverseBtn, hideSettingsPage } from "./set.ui.js";
-displayReverseBtn(false);
-document.getElementById("set-page__reverse-btn")?.addEventListener("click", () => {
-    const state = toggleReverseBtn();
-    displayReverseBtn(state);
-});
-hideSettingsPage();
